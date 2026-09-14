8f293d66b2461399cc456437952b98918548cd73
Matt Ryan <coded.cortex@gmail.com>
2026-09-14 10:12:17 -0400

    Enhance Swagger UI customization by adding padding-bottom to the scheme container for improved layout consistency.

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ app.use(
 );
 const customSwaggerOptions: types.KeyValue = {
 	customCss:
-		".swagger-ui .topbar, .information-container { display: none; } #messageWrapper { max-height: 300px; overflow-y: scroll } .scheme-container { padding-top: 5px !important; }",
+		".swagger-ui .topbar, .information-container { display: none; } #messageWrapper { max-height: 300px; overflow-y: scroll } .scheme-container { padding-top: 5px !important; padding-bottom: 5px !important; }",
 	// customJs: "/swagger.js",
 };
 let localSwaggerJSON = JSON.parse(JSON.stringify(swaggerJSON));
