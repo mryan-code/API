ea386e3e6ea96375046fe430fc359e41b08f25aa
Matt Ryan <coded.cortex@gmail.com>
2026-09-17 08:17:54 -0400

    Implement image upload functionality in PortfolioAction with error handling
    - Added logic to read image files from the specified path and create a Blob for storage.
    - Updated the uploadPicturePuzzleImage method to validate and insert image data into the PicturePuzzleImage model.
    - Included error handling to manage potential upload issues effectively.

--- src/actions/PortfolioAction.ts
+++ src/actions/PortfolioAction.ts
@@ -9,6 +9,8 @@ import * as models from "../models";
 import moment from "moment";
 import { Op, Sequelize, QueryTypes } from "sequelize";
 import { loadEnv } from "../functions/loadEnv";
+import fs from "node:fs";
+import { Blob } from "node:buffer";
 
 // // dayjs
 // import dayjs from "dayjs";
@@ -43,11 +45,39 @@ class PortfolioAction extends GenericAction {
 	};
 
 	uploadPicturePuzzleImage = async (): Promise<any> => {
-		console.log("uploadPicturePuzzleImage", this.parameters);
-		// const picturePuzzleImage = await models.PicturePuzzleImage.create({
-		// 	blob: this.parameters.blob,
-		// 	user_id: this.parameters.user_id,
-		// });
+		// console.log("uploadPicturePuzzleImage", this.parameters);
+
+		try {
+			const imageBuffer = fs.readFileSync(this.parameters.path);
+			const blob = new Blob([imageBuffer], {
+				type: this.parameters.mimetype,
+			});
+			this.sqlObject.blob = blob;
+			this.sqlObject.mime_type = this.parameters.mimetype;
+			this.sqlObject.user_id = this.parameters.user_id;
+			this.sqlObject = {};
+			console.log("sqlObject", this.sqlObject);
+
+			const masterValidation = await validation.validateAll(
+				this,
+				models.PicturePuzzleImage,
+				this.sqlObject,
+				this.parameters,
+			);
+			if (masterValidation) {
+				const insertSQL: any = await models.PicturePuzzleImage.create(
+					this.sqlObject,
+					{
+						logging: (sql: string) => {
+							this.queries.push(sql.toString());
+						},
+					},
+				);
+			}
+		} catch (error) {
+			console.error("Error uploading picture puzzle image", error);
+			throw new Error("Error uploading picture puzzle image");
+		}
 	};
 }
 export { PortfolioAction };
